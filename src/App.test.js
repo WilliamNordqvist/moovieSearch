da12d76f5a65eeaa27a7fc5383d4299d2d734040
William-Insurely <william@insurely.se>
2020-07-30 13:16:25 +0200

    added simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import MainTitle from './componets/title/index'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("should render title", () => {
+  const wrapper = shallow(<MainTitle />);
+  const element = wrapper.find('#title')  
+  expect(element.text()).toBe('Movie Search App');
+
 });
+
--- src/componets/title/index.js
+++ src/componets/title/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as Styled from './style'
 
 const MainTitle = () => {
-  return <Styled.Title>Movie Search App</Styled.Title>
+  return  <Styled.Title id="title">Movie Search App</Styled.Title>
 }
 
 export default MainTitle
